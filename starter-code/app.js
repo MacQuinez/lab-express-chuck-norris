aa73940dbe2f1343a24897cd78df41a3cf7c1f21
Javier Marquinez <emarquinez@gmail.com>
2018-01-09 21:21:05 +0100

    feat: initial commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const app = express();
-const Chuck  = require('chucknorris-io');
+const Chuck = require('chucknorris-io');
 const client = new Chuck();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
 
+app.get('/random', (req, res, next) => {
+  res.render('joke-by-category');
+});
+app.get('/categories', (req, res, next) => {
+  res.render('categories');
+});
+app.get('/search', (req, res, next) => {
+  res.render('');
+});
+app.listen(3000, () => {
+  console.log('My first app listening on port 3000!');
+});
