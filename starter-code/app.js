51a643f4af12930f50b69ba55fa419dc06fdbc67
Javier Marquinez <emarquinez@gmail.com>
2018-01-13 15:25:01 +0100

    feat: first iteration done

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,21 +2,21 @@ const express = require('express');
 const app = express();
 const Chuck = require('chucknorris-io');
 const client = new Chuck();
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.get('/', (req, res, next) => {
-  res.render('index');
-});
 
 app.get('/random', (req, res, next) => {
-  res.render('joke-by-category');
+  client
+    .getRandomJoke()
+    .then(response => {
+      res.render('index', { response });
+    })
+    .catch(err => {
+      console.log('error');
+    });
 });
-app.get('/categories', (req, res, next) => {
-  res.render('categories');
-});
-app.get('/search', (req, res, next) => {
-  res.render('');
-});
+
 app.listen(3000, () => {
   console.log('My first app listening on port 3000!');
 });
